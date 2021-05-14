2281a945c72e36a0854d872ead33cd9fdca3e5ec
ValeriaKrivosheeva <valeriakrivosheeva333@gmail.com>
2021-05-15 00:18:34 +0300

    lab8 in process

---
+++ lab8/program/BinaryMinHeap.cs
@@ -0,0 +1,175 @@
+using System;
+namespace program
+{
+    public class BinaryMinHeap
+    {
+        private string[] heap;
+        private int heapSize;
+        public BinaryMinHeap()
+        {
+            this.heap = new string[7];
+            this.heapSize = 0;
+        }
+        public void Insert(string s)
+        {
+            if(heap.Length == heapSize)
+            {
+                Resize();
+            }
+            int position = heapSize;
+            heap[heapSize] = s;
+            heapSize++;
+            HeapifyBottomToTop(position);
+        }
+        public string ExtractMin()
+        {
+            if(heapSize == 0)
+            {
+                throw new Exception("Cannot extract, heap is empty.");
+            }
+            string min = heap[0];
+            heap[0] = heap[heapSize - 1];
+            heap[heapSize - 1] = min;
+            heapSize--;
+            HeapifyTopToBottom(0);
+            return min;
+        }
+        private void Resize()
+        {
+            Array.Resize(ref heap, heap.Length*2);
+        }
+        private void HeapifyBottomToTop(int i)
+        {
+            int parentIndex = (i-1)/2;
+            if(parentIndex >= 0 && CompareStrings(heap[parentIndex], heap[i]) == 2)
+            {
+                string buffer = heap[parentIndex];
+                heap[parentIndex] = heap[i];
+                heap[i] = buffer;
+                HeapifyBottomToTop(parentIndex);
+            }
+        }
+        private void HeapifyTopToBottom(int i)
+        {
+            int leftChild = 2*i + 1;
+            int rightChild = 2*i + 2;
+            int min = 0;
+            if(leftChild < heapSize)
+            {
+                if(rightChild < heapSize && CompareStrings(heap[leftChild], heap[rightChild]) == 2)
+                {
+                    min = rightChild;
+                }
+                else
+                {
+                    min = leftChild;
+                }
+            }
+            else
+            {
+                return;
+            }
+            if(CompareStrings(heap[min], heap[i]) == 1)
+            {
+                string buffer = heap[min];
+                heap[min] = heap[i];
+                heap[i] = buffer;
+                HeapifyTopToBottom(min);
+            }
+        }
+        public string[] PyramidSort(string[] input, bool control)
+        {
+            if(heap[0] != null)
+            {
+                ClearHeap();
+            }
+            for(int i = 0; i<input.Length; i++)
+            {
+                Insert(input[i]);
+                if(control)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Insert {input[i]}...");
+                    Console.ResetColor();
+                    PrintHeap();
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Formed binary min heap:");
+            Console.ResetColor();
+            PrintHeap();
+            for(int i = 0; i<input.Length; i++)
+            {
+                string extracted = ExtractMin();
+                if(control)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Extract {extracted}...");
+                    Console.ResetColor();
+                    PrintHeap();
+                }
+            }
+            string[] sorted = new string[input.Length];
+            Array.Copy(heap, sorted, input.Length);
+            return sorted;
+        }
+        private void ClearHeap()
+        {
+            this.heap = new string[7];
+            this.heapSize = 0; 
+        }
+
+        public void PrintHeap()
+        {
+            if(heapSize == 0)
+            {
+                Console.WriteLine("Heap is empty.");
+                return;
+            }
+            Console.WriteLine($"Root: {heap[0]}");
+            string whiteSpace = "   ";
+            PrintChildren(0, whiteSpace);
+        }
+        private void PrintChildren(int i, string whiteSpace)
+        {
+            int leftChild = 2*i + 1;
+            int rightChild = 2*i + 2;
+            if(leftChild < heapSize)
+            {
+                Console.WriteLine(whiteSpace + $"L: {heap[leftChild]}");
+                string newWhiteSpace = whiteSpace + "|   ";
+                PrintChildren(leftChild, newWhiteSpace);
+            }
+            if(rightChild < heapSize)
+            {
+                Console.WriteLine(whiteSpace + $"R: {heap[rightChild]}");
+                string newWhiteSpace = whiteSpace + "    ";
+                PrintChildren(rightChild, newWhiteSpace);
+            }
+        }
+        public int CompareStrings(string s1, string s2) //returns number of min string
+        {
+            int minLength = Math.Min(s1.Length, s2.Length);
+            for(int i = 0; i<minLength; i++)
+            {
+                if((int)s1[i] < (int)s2[i])
+                {
+                    return 1;
+                }
+                else if((int)s1[i] > (int)s2[i])
+                {
+                    return 2;
+                }
+            }
+            if(minLength == s1.Length && minLength == s2.Length)
+            {
+                return 0;
+            }
+            else if(minLength == s1.Length)
+            {
+                return 1;
+            }
+            return 2;
+        }
+    }
+}
---
+++ lab8/program/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(@"Available options:
+                1. Control tree
+                2. Enter data for tree
+                3. Exit");
+                Console.ResetColor();
+                string command = Console.ReadLine();
+                if(command == "1")
+                {
+                    string[] control = new string[]{"base", "airport", "queue", "city", "yellow", "anyone", "done", "exercise",
+                                                    "zebra", "pyton", "done", "queue"};
+                    ProcessData(control, true);
+                }
+                else if(command == "2")
+                {
+                    Console.WriteLine("Enter number of strings:");
+                    int n;
+                    while(!int.TryParse(Console.ReadLine(),out n) || n <= 0)
+                    {
+                        Console.WriteLine("Error: Number of strings must be positive integer. Try again.");
+                    }
+                    string[] input = new string[n];
+                    for(int i = 0; i<n; i++)
+                    {
+                        string s = "";
+                        while(true)
+                        {
+                            Console.Write($"Enter {i+1} string: ");
+                            s = Console.ReadLine();
+                            s = s.ToLower();
+                            if(!CheckInput(s))
+                            {
+                                Console.WriteLine("Error: Incorrect input format. Try again.");
+                                continue;
+                            }
+                            break;
+                        }
+                        input[i] = s;
+                    }
+                    ProcessData(input, false);
+                }
+                else if(command == "3")
+                {
+                    Console.WriteLine("End.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Unavailable command. Try again.");
+                    continue;
+                }
+            }
+        }
+        static void ProcessData(string[] input, bool isControl)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Input:");
+            Console.ResetColor();
+            PrintArray(input);
+            BinaryMinHeap heap = new BinaryMinHeap();
+            string[] sorted = heap.PyramidSort(input, isControl);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Sorted:");
+            Console.ResetColor();
+            PrintArray(sorted);
+        }
+        static void PrintArray(string[] array)
+        {
+            for(int i = 0; i<array.Length; i++)
+            {
+                Console.WriteLine("   " + array[i]);
+            }
+        }
+        static bool CheckInput(string s)
+        {
+            if(s.Length == 0)
+            {
+                return false;
+            }
+            for(int i = 0; i<s.Length; i++)
+            {
+                int charCode = (int)s[i];
+                if(charCode < 97 || charCode > 122)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
